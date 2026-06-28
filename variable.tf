049d803418e9570310e4c225759de3bfe744dac9
saurav266 <saurav840963@gmail.com>
2026-06-28 16:09:05 +0530

    var

--- variable.tf
+++ variable.tf
@@ -1,9 +1,9 @@
 variable "ec2_instance_type" {
-    default ="t3.micro"
+  type    = string
+  default = "t3.micro"
 }
 
-variable "ec2_root_storage_size {
-    default =15
-    type =number
-    
+variable "ec2_root_storage_size" {
+  type    = number
+  default = 15
 }
